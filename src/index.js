857094dc989fd23ab745cd032798f90d488808a6
Booze33 <tebetisloh@gmail.com>
2023-04-19 12:00:15 -0500

    test api

--- src/index.js
+++ src/index.js
@@ -12,32 +12,74 @@ const apiDataPokemonDetail = async (number) => {
   }
 };
 
-const display = (detail) => {
+// const display = (detail) => {
+//   const body = document.querySelector('body');
+//   body.classList.add('popup-open');
+//   const popUp = document.createElement('div');
+//   popUp.classList.add('pop-up-container');
+
+//   const popUpContent = `
+//   <img src='${detail.sprites.front_default}'>
+//   <button class="close"><i class="fa fa-window-close" aria-hidden="true"></i></button>
+//   <div>${detail.name}</div>
+//   <p>Height: ${detail.height}</p>
+//   <p>Weight: ${detail.weight}</p>
+//   <p>Abilities: ${detail.abilities.map((ability) => ability.ability.name).join(', ')}</p>
+//   <form>
+//     <input type="text" id="name" placeholder="Enter Name" maxlength="30">
+//     <textarea id="comment" maxlength="500">Write your comment here...</textarea>
+//   </form>
+// `;
+
+//   popUp.classList.add('pop-up-container');
+//   popUp.innerHTML = popUpContent;
+
+//   const closeButton = popUp.querySelector('.close');
+//   closeButton.addEventListener('click', () => {
+//     popUp.classList.add('hidden');
+//   });
+
+//   document.body.appendChild(popUp);
+// };
+
+const display = async (detail) => {
   const body = document.querySelector('body');
   body.classList.add('popup-open');
   const popUp = document.createElement('div');
   popUp.classList.add('pop-up-container');
 
   const popUpContent = `
-  <img src='${detail.sprites.front_default}'>
-  <button class="close"><i class="fa fa-window-close" aria-hidden="true"></i></button>
-  <div>${detail.name}</div>
-  <p>Height: ${detail.height}</p>
-  <p>Weight: ${detail.weight}</p>
-  <p>Abilities: ${detail.abilities.map((ability) => ability.ability.name).join(', ')}</p>
-  <form>
-    <input type="text" id="name" placeholder="Enter Name" maxlength="30">
-    <textarea id="comment" maxlength="500">Write your comment here...</textarea>
-  </form>
-`;
+    <img src='${detail.sprites.front_default}'>
+    <button class="close"><i class="fa fa-window-close" aria-hidden="true"></i></button>
+    <div>${detail.name}</div>
+    <p>Height: ${detail.height}</p>
+    <p>Weight: ${detail.weight}</p>
+    <p>Abilities: ${detail.abilities.map((ability) => ability.ability.name).join(', ')}</p>
+    <div class="comments"></div>
+    <form>
+        <input type="text" id="name" placeholder="Enter Name" maxlength="30">
+        <textarea id="comment" maxlength="500">Write your comment here...</textarea>
+        <button class="comment-button">Comment</button>
+    </form>
+  `;
 
-  popUp.classList.add('pop-up-container');
   popUp.innerHTML = popUpContent;
 
   const closeButton = popUp.querySelector('.close');
   closeButton.addEventListener('click', () => {
     popUp.classList.add('hidden');
   });
+
+  const commentsContainer = popUp.querySelector('.comments');
+  try {
+    const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/ErvLLCGB8PcsVhOAGDFz/comments');
+    const comments = await response.json();
+    commentsContainer.innerHTML = comments.map((comment) => `
+      <div>${comment.username}: ${comment.comment}</div>
+    `).join('');
+  } catch (error) {
+    console.error(error);
+  }
 
   document.body.appendChild(popUp);
 };
